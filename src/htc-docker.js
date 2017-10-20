65e834db9eba6def65c32057662b63cd0167fcdb
arusakov <a_s_rusakov@mail.ru>
2017-10-20 15:31:03 +0300

    recode a lot of things

--- src/htc-docker.js
+++ src/htc-docker.js
@@ -1,43 +1,77 @@
 const path = require('path')
-const sh = require('shelljs')
-// sh.config.verbose = true
 
-const {argv} = require('yargs')
+const sh = require('shelljs')
+
+const { argv } = require('yargs')
   .command('release <register_url>', 'build docker and push to register')
   .help('h')
   .alias('h', 'help')
   .alias('v', 'version')
 
-const getVersionFromPackageJSON = () => {
-  return require(path.join(process.cwd(), 'package.json')).version
+const log = (s) => console.log('htc-docker OK: ' + s)
+const logerr = (s) => console.error('htc-docker ERROR: ' + s)
+const error = (s) => {
+  logerr(s)
+  sh.exit(1)
 }
 
-const echo = (s) => sh.echo('\nhtc-docker: ' + s)
+const getVersionFromPackageJSON = () => {
+  try {
+    return require(path.join(process.cwd(), 'package.json')).version
+  } catch (e) {
+    error('No package.json in cwd')
+  }
+}
+
+const execOrFail = (cmd, msgFail, msgOk = '', okIsNotOk = false) => {
+  let code = !(sh.exec(cmd).code)
+  if (okIsNotOk) {
+    code = !code
+  }
+  if (code) {
+    msgOk && log(msgOk)
+  } else {
+    error(msgFail)
+  }
+}
+
+const execAndFailIfOk = (cmd, msgFail, msgOk) => execOrFail(cmd, msgFail, msgOk, true)
+const exec = (cmd, msg) => sh.exec(cmd).code === 0
+
+// CMDS section
 
 const release = () => {
   const version = getVersionFromPackageJSON()
   const imageURL = argv.register_url + ':' + version
+  log(`Image url is ${imageURL}`)
 
-  if (sh.exec(`docker pull ${imageURL}`).code === 0) {
-    echo(`Docker image ${imageURL} already exist`)
-    sh.exit(1);
-  } else {
-    echo('It\'s OK')
-  }
+  execAndFailIfOk(
+    `docker pull ${imageURL}`,
+    `Docker image ${imageURL} already exist`,
+    'Previous "not found" is OK here',
+  )
+  execOrFail(
+    `docker build -t ${imageURL} .`,
+    `Cannot build image ${imageURL}`
+  )
+  execOrFail(
+    `docker push ${imageURL}`,
+    `Cannot push image ${imageURL}`
+  )
 
-  if (sh.exec(`docker build -t ${imageURL} .`).code !== 0) {
-    echo(`Cannot build image ${imageURL}`)
-    sh.exit(1)
-  }
-
-  if (sh.exec(`docker push ${imageURL}`).code !== 0) {
-    echo(`Cannot push image ${imageURL}`)
-    sh.exit(1)
-  }
+  log(`Image ${imageURL} successfully pushed to register`)
 }
 
+
 // hash with all cmds
-const cmds = { release }
+const cmds = {
+  release,
+}
 
 // just run some cmd
-cmds[argv._[0]]()
+const cmdFunc = cmds[argv._[0]]
+if (cmdFunc) {
+  cmdFunc()
+} else {
+  error('Cmd is not found, use -h or --help for info')
+}
