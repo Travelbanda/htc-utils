46dafb45e569a05daa3c8ee6eae878b8d6ce0146
arusakov <a_s_rusakov@mail.ru>
2017-10-20 15:13:29 +0300

    releasefront

--- src/htc-docker.js
+++ src/htc-docker.js
@@ -1,9 +1,13 @@
+const crypto = require('crypto')
+const fs = require('fs')
 const path = require('path')
 
 const sh = require('shelljs')
+const lockfile = require('@yarnpkg/lockfile')
 
 const { argv } = require('yargs')
   .command('release <register_url>', 'build docker and push to register')
+  .command('releasefront <register_url>', 'build docker for static and push to register')
   .help('h')
   .alias('h', 'help')
   .alias('v', 'version')
@@ -62,10 +66,61 @@ const release = () => {
   log(`Image ${imageURL} successfully pushed to register`)
 }
 
+const readLockFile = () => {
+  try {
+    const lockPath = path.join(process.cwd(), 'yarn.lock')
+    const lockContent = fs.readFileSync(lockPath, 'utf8')
+    return lockfile.parse(lockContent)
+  } catch (e) {
+    error('Cannot find or parse yarn.lock file in cwd')
+  }
+}
+
+const readDockerFile = () => {
+  try {
+    const dockerfilePath = path.join(process.cwd(), 'Dockerfile')
+    return fs.readFileSync(dockerfilePath, 'utf8')
+  } catch (e) {
+    error('Cannot find Dockerfile in cwd or find FROM cmd in Dockerfile')
+  }
+}
+
+const sha1 = (...args) => {
+  const h = crypto.createHash('sha1')
+  for (const a of args) {
+    h.update(a)
+  }
+  return h.digest('hex')
+}
+
+const releasefront = () => {
+  const version = getVersionFromPackageJSON()
+  
+  const dockerFile = readDockerFile()
+
+  const packages = readLockFile().object
+  const packagesSign = Object
+    .keys(packages)
+    .map(k => packages[k].resolved)
+    .join('_$_')
+
+  const baseImageHash = sha1(dockerFile, packagesSign)
+  const baseImageURL = `${argv.register_url}/base/${baseImageHash}`
+  log(`Base image url is ${baseImageURL}`)
+
+  if (!exec(`docker pull ${baseImageURL}`)) {
+    execOrFail(
+      `docker push -t ${baseImageURL}`,
+      `Cannot push ${baseImageURL}`
+    )
+  }
+}
+
 
 // hash with all cmds
 const cmds = {
   release,
+  releasefront,
 }
 
 // just run some cmd
